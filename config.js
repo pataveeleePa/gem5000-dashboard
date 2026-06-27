c88b9d3ed342062c59421ca44204ea44988c3ff3
pataveeleePa <patavee.lee@mahidol.ac.th>
2026-06-27 20:43:46 +0700

    config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 window.GEM5000_CONFIG = {
   SUPABASE_URL: 'https://sncmukjkycpoefacesim.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuY211a2preWNwb2VmYWNlc2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MjMwODMsImV4cCI6MjA5ODA5OTA4M30.uVIbcJCH5xotjpUdHC3ZCQz1caD0yyoY5qhfNTJodGQ',
-  IMPORT_APP_URL: ''
+  IMPORT_APP_URL: 'https://script.google.com/macros/s/AKfycbzUnKcoIPCl5lboYlqMeH6AuJuhdAKLowNQux13TQI0QYoa42w2SQthGDyepTvTK0x7/exec'
 };
